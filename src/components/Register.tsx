b80777728a180c84c8a269b831ebb4ea9903bf41
usman1100 <usmanahmed1100@gail.com>
2022-03-27 15:10:57 +0500

    Register form validation done

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,3 +1,4 @@
+import { useFormik } from "formik";
 import { useState } from "react";
 
 interface RegisterPageProps {
@@ -7,41 +8,97 @@ interface RegisterPageProps {
 function Register({ setAuthPage }: RegisterPageProps) {
   const [newOrg, setNewOrg] = useState<boolean>(false);
 
+  
+  
+
+
+  const formik = useFormik({
+    initialValues:{
+      name:"",
+      email:"",
+      newPassword:"",
+      confirmPassword:"",
+      orgName:"",
+    },
+    validate,
+    onSubmit: (values) => {
+      console.log(values);
+    }
+  })
+
+  function validate({
+    name, newPassword, confirmPassword, email, orgName
+  }:any){
+    const errors:any = {};
+    if (!name || name.length < 3) errors.name = "Name is required";
+    if (!newPassword || newPassword.length < 10) errors.newPassword = "Password is required";
+    if (!confirmPassword || confirmPassword !== newPassword) {
+      errors.confirmPassword = "Confirm Password is required";
+      errors.newPassword = "Confirm Password is required";
+    }
+    if (newOrg && !orgName) errors.orgName = "Organization Name is required";
+    return errors;
+  }
+
   return (
     <>
       <h1 className="font-bold text-3xl mb-5">Register</h1>
 
       <form
         className="form-control flex w-2/3 mx-auto"
-        onSubmit={(e) => e.preventDefault()}
+        onSubmit={formik.handleSubmit}
       >
         <div className="grid grid-cols-2 items-center gap-5">
           <input
-            className="input input-bordered input-success"
+            required
+            className={
+              `input input-bordered 
+              ${formik.touched.name && formik.errors.name ? "input-error" : "input-success"}`
+            }
             placeholder="Enter your full name"
             type="text"
             name="name"
+            value={formik.values.name}
+            onChange={formik.handleChange}
           />
           <input
-            className="input input-bordered input-success"
+            required
+            className={
+              `input input-bordered 
+              ${formik.touched.email && formik.errors.email ? "input-error" : "input-success"}`
+            }
             placeholder="Enter an email address"
             type="email"
             name="email"
             autoComplete="email"
           />
           <input
-            className="input input-bordered input-success "
+            required
+            className={
+              `input input-bordered 
+              ${formik.touched.newPassword && formik.errors.newPassword ? "input-error" : "input-success"}`
+            }
             placeholder="Create a password"
             type="password"
-            name="password"
+            name="newPassword"
             autoComplete="new-password"
+            id="newPassword"
+            value={formik.values.newPassword}
+            onChange={formik.handleChange}  
           />
           <input
-            className="input input-bordered input-success "
+            required
+            className={
+              `input input-bordered 
+              ${formik.touched.confirmPassword && formik.errors.confirmPassword ? "input-error" : "input-success"}`
+            }
             placeholder="Confirm your password"
             type="password"
-            name="password"
+            name="confirmPassword"
+            id="confirmPassword"
             autoComplete="new-password"
+            value={formik.values.confirmPassword}
+            onChange={formik.handleChange}
           />
           <div className="form-control">
             <label className="label cursor-pointer">
@@ -61,7 +118,11 @@ function Register({ setAuthPage }: RegisterPageProps) {
 
           {newOrg && (
             <input
-              className="input input-bordered input-success "
+              required
+              className={
+                `input input-bordered 
+                ${formik.touched.orgName && formik.errors.orgName ? "input-error" : "input-success"}`
+              }
               placeholder="Enter your organization name"
               type="text"
               name="organization-name"
