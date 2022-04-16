d3b47fdd14cc12e8f5597f3cbde4a88aa2c1b30e
usman1100 <usmanahmed1100@gail.com>
2022-04-16 15:22:04 +0500

    Seprarted Routes

---
+++ src/pages/AppRoutes.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import NotFound from './404'
+import Auth from './Auth'
+import Dashboard from './Dashboard'
+import Organization from './Organizations'
+import Settings from './Settings'
+
+export default function AppRoutes() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Auth />} />
+				<Route path='/dashboard' element={<Dashboard />}>
+					<Route
+						path='/dashboard/organization'
+						element={<Organization />}
+					/>
+
+					<Route
+						path='/dashboard/organization/:orgId'
+						element={<>hello</>}
+					/>
+
+					<Route path='/dashboard/projects' element={<>Projects</>} />
+
+					<Route path='/dashboard/setting' element={<Settings />} />
+				</Route>
+				<Route path='*' element={<NotFound />} />
+			</Routes>
+		</BrowserRouter>
+	)
+}
