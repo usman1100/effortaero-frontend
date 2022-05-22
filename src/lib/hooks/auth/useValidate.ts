6635e3aeb1e69f3a25cb466aa333015ec2789f15
usman1100 <usmanahmed1100@gmail.com>
2022-05-22 18:30:26 +0500

    Validation time increased to 15 minutes

--- src/lib/hooks/auth/useValidate.ts
+++ src/lib/hooks/auth/useValidate.ts
@@ -5,7 +5,7 @@ export default function useValidate() {
 	const authAPI = new AuthService()
 
 	return useQuery('validate', authAPI.validate, {
-		refetchInterval: 1000 * 60,
+		refetchInterval: 1000 * 60 * 15,
 		refetchOnWindowFocus: false,
 		retry: 1,
 	})
